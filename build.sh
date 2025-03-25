ed7e8ebec0b9c0ea31969a8c3c5595efa5b79bda
Kirt <kirt.quar@hotmail.com>
2025-03-25 12:02:59 +1100

    Update build.sh to create static directory

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist
+mkdir -p static
+mkdir -p staticfiles
+
 python manage.py collectstatic --no-input
 python manage.py migrate
