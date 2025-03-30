c7d19a82e5e7d052d2fde42f725603f405309e3d
kirtq <kirt.quar@gmail.com>
2025-03-30 21:14:56 +1100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = 'core'
 
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'schools', views.SchoolViewSet)
+router.register(r'classes', views.SchoolClassViewSet)
+router.register(r'students', views.StudentViewSet)
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/', include(router.urls)),
 ]
